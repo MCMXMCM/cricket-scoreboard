2e1466dbb8894b58a4a22ece3280f49c8df600f2
Matthew McCarty <matthew.mccarty@daugherty.com>
2022-10-29 21:32:19 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import {
+  AppBar,
   Grid,
   Box,
   BottomNavigation,
@@ -28,76 +29,87 @@ function App() {
 
   return (
     <div className="App">
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        flexDirection="column"
-        backgroundColor="secondary.dark"
-      >
-        <Grid
-          container
-          spacing={1}
-          sx={{
-            textAlign: "center",
-            color: "white",
-          }}
+      <Box height="100vh" backgroundColor="secondary.dark">
+        <Box
+          height="90vh"
+          display="flex"
+          justifyContent="center"
+          flexDirection="column"
+          backgroundColor="secondary.dark"
         >
-          <Grid item xs={4}>
-            <Typography variant="h3">Player 1</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h3">Cricket</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h3">Player 2</Typography>
+          <AppBar position="static">
+            <Typography
+              sx={{ marginTop: "1em, 0" }}
+              align="center"
+              variant="h3"
+              component="div"
+            >
+              Simple Cricket Scoreboard
+            </Typography>
+          </AppBar>
+          <Grid
+            container
+            spacing={1}
+            sx={{
+              textAlign: "center",
+              color: "white",
+            }}
+          >
+            <Grid item xs={4}>
+              <Typography variant="h3">Player 1</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3">Cricket</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3">Player 2</Typography>
+            </Grid>
+
+            {numbers.map((number) => (
+              <Row
+                number={number}
+                playerScore={playerOneTotal}
+                playerTwoScore={playerTwoTotal}
+                setPlayerScore={setPlayerOneTotal}
+                setPlayerTwoScore={setPlayerTwoTotal}
+                resetClicker={resetClicker}
+                setResetClicker={setResetClicker}
+              />
+            ))}
+
+            <Grid item xs={4}>
+              <Typography variant="h3">{playerOneTotal}</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3">Totals</Typography>
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h3">{playerTwoTotal}</Typography>
+            </Grid>
           </Grid>
 
-          {numbers.map((number) => (
-            <Row
-              number={number}
-              playerScore={playerOneTotal}
-              playerTwoScore={playerTwoTotal}
-              setPlayerScore={setPlayerOneTotal}
-              setPlayerTwoScore={setPlayerTwoTotal}
-              resetClicker={resetClicker}
-              setResetClicker={setResetClicker}
-            />
-          ))}
-
-          <Grid item xs={4}>
-            <Typography variant="h3">{playerOneTotal}</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h3">Totals</Typography>
-          </Grid>
-          <Grid item xs={4}>
-            <Typography variant="h3">{playerTwoTotal}</Typography>
-          </Grid>
-        </Grid>
-
-        <BottomNavigation
-          sx={{
-            backgroundColor: "secondary.dark",
-            color: "white",
-            marginTop: 5,
-          }}
-          showLabels
-        >
-          {/* <BottomNavigationAction
+          <BottomNavigation
+            sx={{
+              backgroundColor: "secondary.dark",
+              color: "white",
+              marginTop: 5,
+            }}
+          >
+            {/* <BottomNavigationAction
             label="Back"
             icon={<ArrowBackIcon color="tertiary" fontSize="large" />}
           /> */}
-          <BottomNavigationAction
-            onClick={resetGame}
-            label="Reset"
-            icon={<RestartAltIcon color="tertiary" fontSize="large" />}
-          />
-          {/* <BottomNavigationAction
+            <BottomNavigationAction
+              onClick={resetGame}
+              label="Reset"
+              icon={<RestartAltIcon color="tertiary" fontSize="large" />}
+            />
+            {/* <BottomNavigationAction
             label="Share"
             icon={<ArrowForwardIcon color="tertiary" fontSize="large" />}
           /> */}
-        </BottomNavigation>
+          </BottomNavigation>
+        </Box>
       </Box>
     </div>
   );
